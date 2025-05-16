5032a9a46fae97d27feffbd3934a18a0063d5682
Big1620 <danielsalinas200302@gmail.com>
2025-05-16 07:16:42 -0500

    Se cambio el middleware

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -47,6 +47,7 @@ ACCOUNT_UNIQUE_EMAIL = True
 # Middleware
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -54,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
+    
 ]
 
 # URLs
